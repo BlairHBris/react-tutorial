e1fdb1ff9232c67935819c808a8bf3d0e116916c
blair <blairhbristow@gmail.com>
2022-08-26 11:14:08 -0400

    Add base for tasks and associated styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react"
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: "Sign house contract",
+        day: "August 26th",
+        reminder: false
+    },
+    {
+        id: 2,
+        text: "House inspection",
+        day: "August 30th",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Closing",
+        day: "September 29th",
+        reminder: false
+    }
+])
 
   return (
     <div className="container">
       <Header/>
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
